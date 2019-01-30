71fa7410c93e52a777854073f99cf22f99a4304a
Gayatri <ggayatri29@gmail.com>
2019-01-29 21:14:52 -0600

    added new files

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const app = express();
+const knex = require('knex')
+const register = require('./controllers/register')
+const signin = require('./controllers/signin')
+const profile = require('./controllers/profile')
+const image = require('./controllers/image')
+
+
+/*const db = knex({
+  client: 'pg',
+  connection: {
+    host : 'postgresql-corrugated-50642',
+    user : 'postgres',
+    password : 'test',
+    database : 'smartbrain'
+  }
+});
+*/
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    connectionString : process.env.DATABASE_URL,
+    ssl:true
+  }
+});
+
+
+app.use(bodyParser.json());
+app.use(cors());
+
+
+app.get('/', (req,res) => res.json("It is working"))
+
+app.post('/signin', (req,res) => signin.handleSignin(req,res,db,bcrypt))
+
+app.post('/register', (req,res) => register.handleRegister(req,res,db, bcrypt))
+
+app.get('/profile/:id', (req,res) => profile.handleProfile(req,res,db))
+
+
+app.put('/image',  (req,res) => image.handleImage(req,res,db))
+ 
+app.post('/imageurl',  (req,res) => image.handleApiCall(req,res))
+ 
+
+app.listen(process.env.PORT || 3000,()=>{
+	console.log('app is running on ${process.env.PORT}');
+})
+
+
+ 
